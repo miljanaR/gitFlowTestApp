fff09416363267d4cbddd4719bfb396add38ecc7
miljanalf <58689921+miljanalf@users.noreply.github.com>
2020-12-23 23:36:35 +0100

    init

---
+++ gitFlowTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gitFlowTestApp
+//
+//  Created by Miljana Ratkov on 23/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
